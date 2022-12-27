572ace50fd226b4b5edad4683cca701a758777a6
Valmiki Vishwakarma <you@example.com>
2022-12-28 01:06:32 +0530

    Adding code changes.

--- RestAssured27Dec2022/src/test/java/RestAssuredBasics/GetRequest.java
+++ RestAssured27Dec2022/src/test/java/RestAssuredBasics/GetRequest.java
@@ -1,12 +1,45 @@
 package RestAssuredBasics;
 
+import static io.restassured.RestAssured.given;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GetRequest {
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
+public class GetRequest {
+//Below exception is raised when hit the API
+//request when there is No internet connection
+//java.net.UnknownHostException: No such host is known 
+//(restful-booker.herokuapp.com)
 	@Test
 	public void restAssuredGet() {
+		RestAssured.baseURI="https://restful-booker.herokuapp.com";
+	
+		Response response=
+		given()
+		.when().get("/booking/1");
+		//.then().assertThat().statusCode(200);
 		
+		//response.prettyPrint();
+		//Convert response as a String
+	
+	String reponseString=	response.asString();
+	System.out.println(reponseString);
+//{"firstname":"Sally","lastname":"Brown","totalprice":570,"depositpaid":false,"bookingdates":{"checkin":"2019-02-15","checkout":"2020-12-01"}}
+	System.out.println(response.getStatusCode());
+	System.out.println(response.statusCode());
+	System.out.println(response.getStatusLine());
+	System.out.println(response.statusLine());
+	Assert.assertEquals(response.getStatusCode(),200);
+	Assert.assertEquals(response.getStatusLine(), "HTTP/1.1 200 OK");
+/*
+	200
+	200
+	HTTP/1.1 200 OK
+	HTTP/1.1 200 OK */
+	
 	}
 
 }
---
+++ RestAssured27Dec2022/src/test/java/RestAssuredBasics/RunOneTestNGTest.java
@@ -0,0 +1,19 @@
+package RestAssuredBasics;
+
+import org.testng.annotations.Test;
+
+public class RunOneTestNGTest {
+
+	@Test
+	public void add() {
+		System.out.println("Running add test");
+	}
+	
+	@Test
+	public void delete() {
+		System.out.println("Running delete test");
+	}
+	
+	
+	
+}
